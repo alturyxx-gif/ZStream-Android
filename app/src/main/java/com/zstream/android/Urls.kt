690da1c6a83bdf7d054d452cc5cbc337abe5107f
alturyxx-gif <alturyxx-gif@users.noreply.github.com>
2026-07-08 15:40:53 -0700

    Add proper manifest JSON URL

--- app/src/main/java/com/zstream/android/Urls.kt
+++ app/src/main/java/com/zstream/android/Urls.kt
@@ -5,7 +5,7 @@ object Urls {
     const val TMDB_BASE = "https://api.themoviedb.org/3/"
     const val TMDB_IMAGE = "https://image.tmdb.org/t/p/"
     const val IMDB_GRAPHQL = "https://api.graphql.imdb.com/"
-    const val PLUGIN_MANIFEST = "" // INSERT PLUGIN MANIFEST URL HERE
+    const val PLUGIN_MANIFEST = "https://raw.githubusercontent.com/alturyxx-gif/zstream-plugin-releases/main/manifest.json"
     const val APP_GITHUB_REPO = "https://github.com/alturyxx-gif/ZStream-Android"
     const val DISCORD_LINK = "https://discord.com/invite/wmbWfk4SGy"
     const val NOTIFICATIONS_RSS = "https://zstream.mov/notifications.xml"
