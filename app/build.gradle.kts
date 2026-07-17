835eea301c60241df52990f304d34276520fc65e
alturyxx-gif <alturyxx-gif@users.noreply.github.com>
2026-07-16 21:25:23 -0700

    Bump app version to v1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "v1.4"
+        versionName = "v1.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val props = Properties()
